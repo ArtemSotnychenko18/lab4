619d6e468ca0c639218d85a5c77417330e51853f
ArtemSotnychenko18 <kn23-a.sotnychenko@nubip.edu.ua>
2024-11-12 12:44:35 +0200

    Update task3.js

--- lab4-main/tasks/task3.js
+++ lab4-main/tasks/task3.js
@@ -1,15 +1,11 @@
 
 function sumArray(numbers) {
-	let sum = 0; 
-	
-	for (let number of numbers) {
-	  sum += number; 
-	}
-	
-	return sum; 
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
   }
-
+  return sum;
+}
 console.log(sumArray([1, 2, 3, 4, 5])); 
-console.log(sumArray([10, 20, 30]));    
-
+console.log(sumArray([10, 20, 30]));   
 module.exports = sumArray;
