f064d3e9c6b6854709b65ff006c045d21f03b54a
ArtemSotnychenko18 <kn23-a.sotnychenko@nubip.edu.ua>
2024-11-12 12:43:48 +0200

    Update task2.js

--- lab4-main/tasks/task2.js
+++ lab4-main/tasks/task2.js
@@ -1,16 +1,13 @@
 function reverseWithoutNumbers(str) {
-	const chars = [];
-	
-	for (let char of str) {
-	  if (!(char >= '0' && char <= '9')) {
-		chars.push(char);
-	  }
-	}
-	
-	return chars.reverse().join('');
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    let char = str[i];
+    if (isNaN(char) || char === ' ') {
+      result += char;
+    }
   }
-
+  return result;
+}
 console.log(reverseWithoutNumbers("hello123world456")); 
-console.log(reverseWithoutNumbers("abc123xyz"));       
-
+console.log(reverseWithoutNumbers("abc123xyz"));      
 module.exports = reverseWithoutNumbers;
